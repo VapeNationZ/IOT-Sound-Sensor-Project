2f65a367bdb0c31f1f93e633017df2ca595e8177
Joslyn Pereira <31210814+VapeNationZ@users.noreply.github.com>
2018-09-22 22:12:33 +0530

    Create soundsens3.ino

---
+++ soundsens3.ino
@@ -0,0 +1,21 @@
+const int ledpin=13; // ledpin and soundpin are not changed throughout the process 
+const int soundpin=A2;
+const int threshold=200; // sets threshold value for sound sensor
+void setup() {
+Serial.begin(9600);
+pinMode(ledpin,OUTPUT);
+pinMode(soundpin,INPUT);
+}
+void loop() {
+int soundsens=analogRead(soundpin); // reads analog data from sound sensor
+if (soundsens>=threshold) {
+Serial.print("Threshold exceeded, Threshold =");
+Serial.println(soundsens);
+Serial.print("\t");
+digitalWrite(ledpin,HIGH); //turns led on
+delay(0);
+}
+else{
+digitalWrite(ledpin,LOW);
+}
+}
